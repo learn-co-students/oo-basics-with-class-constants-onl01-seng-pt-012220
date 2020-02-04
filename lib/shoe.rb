23cbe0f4bf23242976bf8fc1d121fcf5f4f8e72b
Samantha Jordan <shinyguns@gmail.com>
2020-02-04 08:47:59 -0600

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,14 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = [] #array for brands
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand) #if else to include/exclude genres already there
+    else
+      BRANDS << brand
+    end
   end
 
   def cobble
